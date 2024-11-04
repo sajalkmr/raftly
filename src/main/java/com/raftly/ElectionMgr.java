bb72f4ded6cfe6ee58262eb1c30456bf80029142
Sajal <sajal.kumar2003@gmail.com>
2024-11-04 14:09:46 +0530

    Update ElectionMgr.java

--- src/main/java/com/raftly/ElectionMgr.java
+++ src/main/java/com/raftly/ElectionMgr.java
@@ -1,5 +1,27 @@
 package main.java.com.raftly;
 
+import java.util.List;
+
 public class ElectionMgr {
-    
+    private List<RaftNode> cluster;
+
+    public ElectionMgr(List<RaftNode> cluster) {
+        this.cluster = cluster;
+    }
+
+    public void startElection() {
+        // start an election
+        // Increment current term, vote for self, send RequestVote RPCs
+    }
+
+    public void receiveVote(int voterId) {
+        //  handle votes
+        // Count votes and determine if a leader can be elected
+    }
 }
+
+
+    
+
+    
+
